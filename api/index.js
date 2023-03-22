eb173a4cc159c98b934eea700ef1aa7fc8fc900a
Vishwas1687 <97346738+Vishwas1687@users.noreply.github.com>
2023-03-22 19:40:20 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -103,34 +103,43 @@ app.get('/post/:id',async (req,res)=>{
     res.json(postDoc)
 })
 
-app.put('/post',uploadMiddleWare.single('file'),async(req,res)=>{
-    let newPath=null
-     if(req.file){
-         const {originalname,path}=req.file;
-         const parts=originalname.split('.');
-         const ext=parts[parts.length-1]
-         newPath=path+'.'+ext
-         fs.renameSync(path,newPath)
-     }
+app.put('/post', uploadMiddleWare.single('file'), async (req, res) => {
+  let newPath = null;
 
-      const {token}=req.cookies
-      jwt.verify(token,secret,{},async(err,info)=>{
-        if(err) throw err;
-         const {id,title,summary,content}=req.body
-        const postDoc=await Post.findById(id)
-        const isAuthor=JSON.stringify(postDoc.author)===JSON.stringify(info.id)
-        if(!isAuthor){
-            return res.status(400).json('you are not the author')
-        }
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+  }
 
-        await postDoc.update({
-            title,
-            summary,
-            content,
-            cover:newPath?newPath:postDoc.cover})
-            res.json(postDoc)
-    })
-    
-})
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+
+    try {
+      const postDoc = await Post.findById(id);
+      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+
+      if (!isAuthor) {
+        return res.status(400).json('you are not the author');
+      }
+
+      postDoc.title = title;
+      postDoc.summary = summary;
+      postDoc.content = content;
+      postDoc.cover = newPath ? newPath : postDoc.cover;
+
+      await postDoc.save(); // use save() instead of update()
+
+      res.json(postDoc);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('An error occurred while updating the post.');
+    }
+  });
+});
 app.listen(4000)
 
